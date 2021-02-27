1567aeb37a9bc448bd1ba9f6517c601a019c148b
Emerick94 <lucas.emerick94@gmail.com>
2021-02-27 13:56:18 -0300

    fix users factory

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -2,7 +2,7 @@ FactoryBot.define do
   factory :user do
     displayName { 'Joe' }
     image { 'http://4.bp.blogspot.com/_YA50adQ-7vQ/S1gfR_6ufpI/AAAAAAAAAAk/1ErJGgRWZDg/S45/brett.png' }
-    password_digest { 'test1234' }
+    password { 'test1234' }
     sequence(:email) { |n| "#{displayName}_#{n}@example.com" }
   end
 end
