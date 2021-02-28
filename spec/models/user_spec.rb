b74ea153e203ae5a9ac039a4df3458bc35fbf8b8
Emerick94 <lucas.emerick94@gmail.com>
2021-02-27 21:11:05 -0300

    add spec for user validations

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,72 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it 'is valid with valid attributes' do
+    user = described_class.new(
+      displayName: 'rubens silva',
+      email: 'rubinho2@gmail.com',
+      password: '123456'
+    )
+    expect(user).to be_valid
+  end
+
+  it 'is not valid without a displayName' do
+    user = described_class.new(
+      email: 'rubinho2@gmail.com',
+      password: '123456'
+    )
+    expect(user).to be_invalid
+  end
+
+  it 'is not valid with a displayName shorter than 8 characters' do
+    user = described_class.new(
+      displayName: 'rubens',
+      email: 'rubinho2@gmail.com',
+      password: '123456'
+    )
+    expect(user).to be_invalid
+  end
+
+  it 'is not valid without an email' do
+    user = described_class.new(
+      displayName: 'rubens silva',
+      password: '123456'
+    )
+    expect(user).to be_invalid
+  end
+
+  it 'is not valid with an email lacking the username' do
+    user = described_class.new(
+      displayName: 'rubens silva',
+      email: 'rubinho2',
+      password: '123456'
+    )
+    expect(user).to be_invalid
+  end
+
+  it 'is not valid with an email lacking the domain' do
+    user = described_class.new(
+      displayName: 'rubens silva',
+      email: '@gmail.com',
+      password: '123456'
+    )
+    expect(user).to be_invalid
+  end
+
+  it 'is not valid without a password' do
+    user = described_class.new(
+      email: 'rubinho2@gmail.com',
+      displayName: 'rubens silva'
+    )
+    expect(user).to be_invalid
+  end
+
+  it 'is not valid with a password shorter than 6 characters' do
+    user = described_class.new(
+      email: 'rubinho2@gmail.com',
+      displayName: 'rubens silva',
+      password: '12345'
+    )
+    expect(user).to be_invalid
+  end
 end
