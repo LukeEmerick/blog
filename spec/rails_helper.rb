b104aaadf8d40101921b34f81197ffda08649d2d
Emerick94 <lucas.emerick94@gmail.com>
2021-02-28 13:52:13 -0300

    and jbuilder and config

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -35,6 +35,9 @@ RSpec.configure do |config|
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
 
+  # so we can test jbuilder rendered responses
+  config.render_views = true
+
   # If you're not using ActiveRecord, or you'd prefer not to run each of your
   # examples within a transaction, remove the following line or assign false
   # instead of true.
