b4efdee7a80884d450464fe578540bc15d091627
Emerick94 <lucas.emerick94@gmail.com>
2021-02-27 21:11:49 -0300

    fix factory with new validations

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,8 +1,8 @@
 FactoryBot.define do
   factory :user do
-    displayName { 'Joe' }
+    displayName { 'rubens silva' }
     image { 'http://4.bp.blogspot.com/_YA50adQ-7vQ/S1gfR_6ufpI/AAAAAAAAAAk/1ErJGgRWZDg/S45/brett.png' }
     password { 'test1234' }
-    sequence(:email) { |n| "#{displayName}_#{n}@example.com" }
+    sequence(:email) { |n| "user_#{n}@example.com" }
   end
 end
