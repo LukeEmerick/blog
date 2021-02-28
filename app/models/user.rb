3f097bcd23caa6c888e38c2303fc8da2da275725
Emerick94 <lucas.emerick94@gmail.com>
2021-02-27 21:11:23 -0300

    add user validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,10 @@ class User < ApplicationRecord
   has_secure_password
 
   has_many :posts, dependent: :destroy
+
+  validates :password, length: { minimum: 6, message: 'length must be at least 6 characters long' },
+                       if: :password_digest_changed?
+
+  validates :displayName, length: { minimum: 8, message: 'length must be at least 8 characters long' }
+  validates :email, uniqueness: true, format: { with: /\A(.+)@(.+)\Z/, message: '"email" must be a valid email' }
 end
